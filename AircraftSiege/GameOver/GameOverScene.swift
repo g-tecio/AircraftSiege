176803c7c9d13f40451d48baaed8e2dd5ea61724
Herbeyg Robledo <herbeyg.robledo@g-tec.io>
2018-07-24 13:20:56 -0500

    Initial commit

---
+++ AircraftSiege/GameOver/GameOverScene.swift
@@ -0,0 +1,40 @@
+//
+//  GameOverScene.swift
+//  SpacegameReloaded
+//
+//  Created by Herbeyg Robledo Reyes on 7/23/18.
+//  Copyright © 2018 Training. All rights reserved.
+//
+
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    var score:Int = 0
+    
+    var scoreLabel:SKLabelNode!
+    var newGameButtonNode:SKSpriteNode!
+
+    override func didMove(to view: SKView) {
+        scoreLabel = self.childNode(withName: "scoreLabel") as! SKLabelNode
+        scoreLabel.text = "\(score)"
+        
+        newGameButtonNode = self.childNode(withName: "newGameButton") as! SKSpriteNode
+        newGameButtonNode.texture = SKTexture(imageNamed: "newGameButton")
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touches = touches.first
+        
+        if let location = touches?.location(in: self){
+            let node = self.nodes(at:location)
+            
+            if node[0].name == "newGameButton" {
+                let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+                let gameScene = GameScene(size: self.size)
+                self.view?.presentScene(gameScene, transition: transition)
+            }
+        }
+    }
+}
