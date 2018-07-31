447982f694936f25c160c3e7b3181038efd52d3e
Herbeyg Robledo <herbeyg.robledo@g-tec.io>
2018-07-31 14:48:20 -0500

    -Working on dynamic background

--- AircraftSiege/Game/GameScene.swift
+++ AircraftSiege/Game/GameScene.swift
@@ -17,6 +17,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     var starfield:SKEmitterNode!
+    var smallClouds:SKEmitterNode!
+    var mediumClouds:SKEmitterNode!
     var player:SKSpriteNode!
     var scoreLabel:SKLabelNode!
     var timerLabel:SKLabelNode!
@@ -58,13 +60,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         addLives()
         
-        starfield = SKEmitterNode(fileNamed: "Starfield")
+        starfield = SKEmitterNode(fileNamed: "big_clouds")
         starfield.position = CGPoint(x: 0, y: 1472)
         starfield.advanceSimulationTime(10)
-   
         self.addChild(starfield)
-        
         starfield.zPosition = -1
+        
+        smallClouds = SKEmitterNode(fileNamed: "small_clouds")
+        smallClouds.position = CGPoint(x: 0, y: 1472)
+        smallClouds.advanceSimulationTime(10)
+        self.addChild(smallClouds)
+        smallClouds.zPosition = -1
+        
+        
+        mediumClouds = SKEmitterNode(fileNamed: "medium_clouds")
+        mediumClouds.position = CGPoint(x: 0, y: 1472)
+        mediumClouds.advanceSimulationTime(10)
+        self.addChild(mediumClouds)
+        mediumClouds.zPosition = -1
+        
         
         player = SKSpriteNode(imageNamed: "shuttle")
         
@@ -116,7 +130,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
  */
 
-        self.backgroundColor=UIColor(red:0.54, green:0.90, blue:1.00, alpha:1.0)
+        self.backgroundColor=UIColor(red:0.40, green:0.78, blue:0.92, alpha:1.0)
         gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
         
         print("tiempo\(timeInterval)")
@@ -140,14 +154,63 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //ADD THIS METHOD
     @objc func clock() {
         seconds=seconds+1
-       // timerLabel.text=String(seconds)
+        print(seconds)
+        
+        
+        
+        for _ in seconds...seconds+10{
+             self.backgroundColor=UIColor(red:0.40 - 0.01 , green:0.78 - 0.02, blue:0.92 - 0.02, alpha:1.0)
+            
+        }
+        
+        
+        
+//        switch seconds {
+//        case 5:
+//            self.backgroundColor=UIColor(red:0.40, green:0.78, blue:0.92, alpha:1.0)
+//        case 10:
+//            self.backgroundColor=UIColor(red:0.38, green:0.74, blue:0.88, alpha:1.0)
+//        case 15:
+//            self.backgroundColor=UIColor(red:0.36, green:0.70, blue:0.84, alpha:1.0)
+//        case 20:
+//            self.backgroundColor=UIColor(red:0.34, green:0.66, blue:0.80, alpha:1.0)
+//        case 30:
+//            self.backgroundColor=UIColor(red:0.32, green:0.62, blue:0.76, alpha:1.0)
+//        case 35:
+//            self.backgroundColor=UIColor(red:0.30, green:0.58, blue:0.72, alpha:1.0)
+//        case 40:
+//            self.backgroundColor=UIColor(red:0.28, green:0.54, blue:0.68, alpha:1.0)
+//        case 45:
+//            self.backgroundColor=UIColor(red:0.26, green:0.50, blue:0.64, alpha:1.0)
+//        case 50:
+//            self.backgroundColor=UIColor(red:0.24, green:0.46, blue:0.60, alpha:1.0)
+//        case 55:
+//            self.backgroundColor=UIColor(red:0.22, green:0.42, blue:0.56, alpha:1.0)
+//        default:
+//           print(seconds)
+//        }
+
+        
+       //timerLabel.text=String(seconds)
+        
+        
+        
+        
+        
+        /*
         if (seconds==5){
              self.backgroundColor=UIColor(red:0.46, green:0.84, blue:0.89, alpha:1.0)
                //  starfield.particleSpeed = 500
-            
-           print(starfield.particleScale)
+              // print(starfield.particleScale)
         }
+ 
+ */
     }
+    
+    
+    
+
+    
     
     func random(min: Double, max: Double) -> Double {
         return Double(arc4random_uniform(UInt32(max - min + 1))) + min
@@ -169,7 +232,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     @objc func addAlien () {
         possibleAliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as! [String]
-        starfield.particleScale = CGFloat(random(min: 1, max: 3))
+       // starfield.particleScale = CGFloat(random(min: 1, max: 3))
         let alien = SKSpriteNode(imageNamed: possibleAliens[0])
         
         let randomAlienPosition = GKRandomDistribution(lowestValue: 10, highestValue: 380)
